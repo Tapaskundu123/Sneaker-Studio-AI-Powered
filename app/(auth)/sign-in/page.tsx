c36bb7af2f22ad19b267f3f446811c94f46a3ee2
Tapas kundu <tapaskundu3762@gmail.com>
2025-11-07 21:24:22 +0530

    assets and signup and signin page added

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,16 @@
+// import AuthForm from "@/components/AuthForm";
+// import {signIn} from "@/lib/auth/actions";
+
+// export default function Page() {
+//   return <AuthForm mode="sign-in" onSubmit={signIn} />;
+// }
+
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default page
---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,16 @@
+// import AuthForm from "@/components/AuthForm";
+// import {signUp} from "@/lib/auth/actions";
+
+// export default function Page() {
+//   return <AuthForm mode="sign-up" onSubmit={signUp} />;
+// }
+
+import React from 'react'
+
+const page = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default page
