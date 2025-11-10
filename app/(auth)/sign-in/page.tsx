c67fbb0d07696d0f5f94b1052986506369d6b5a7
Tapas kundu <tapaskundu3762@gmail.com>
2025-11-10 15:30:05 +0530

    fix onSubmit error

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,11 +1,9 @@
-// app/auth/sign-in/page.tsx
 import AuthForm from "@/components/AuthForm";
-import { signIn } from "@/lib/auth/actions";
 
 export default function SignInPage() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-slate-50 via-orange-50 to-slate-100 p-4">
-      <AuthForm mode="sign-in" onSubmit={signIn} />
+      <AuthForm mode="sign-in" />
     </div>
   );
 }
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,11 +1,10 @@
 // app/auth/sign-up/page.tsx
 import AuthForm from "@/components/AuthForm";
-import { signUp } from "@/lib/auth/actions";
 
 export default function SignUpPage() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-slate-50 via-orange-50 to-slate-100 p-4">
-      <AuthForm mode="sign-up" onSubmit={signUp} />
+    <div className="min-h-screen flex items-center justify-center bg-gradiant-to-br from-slate-50 via-orange-50 to-slate-100 p-4">
+      <AuthForm mode="sign-up"  />
     </div>
   );
 }
